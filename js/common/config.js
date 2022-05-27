92780d8f3bd319d79577c3514ff3adeb81cc9669
Jason1997 <921509429@qq.com>
2022-05-28 01:34:39 +0800

    新增elastic官网

--- js/common/config.js
+++ js/common/config.js
@@ -2,44 +2,44 @@
 const config = {
     title: '大森的主页',
     nickname: '大森',
-    favouriteLinks:[
+    favouriteLinks: [
         {
-            type:"常用网站",
+            type: '常用网站',
             links: [
                 {
                     title: '百度翻译',
-                    url: 'https://fanyi.baidu.com/'
+                    url: 'https://fanyi.baidu.com/',
                 },
                 {
                     title: 'QQ邮箱',
-                    url: 'https://mail.qq.com/'
+                    url: 'https://mail.qq.com/',
                 },
                 {
                     title: '哔哩哔哩',
-                    url: 'https://www.bilibili.com/'
+                    url: 'https://www.bilibili.com/',
                 },
                 {
                     title: '阿里云',
-                    url: 'https://www.aliyun.com/'
+                    url: 'https://www.aliyun.com/',
                 },
                 {
                     title: '腾讯云',
-                    url: 'https://cloud.tencent.com//'
+                    url: 'https://cloud.tencent.com//',
                 },
-            ]
+            ],
         },
         {
-            type:"素材查询",
+            type: '素材查询',
             links: [
                 {
                     title: '阿里iconfont',
                     url: 'https://www.iconfont.cn/',
-                    icon: 'https://img.alicdn.com/imgextra/i2/O1CN01ZyAlrn1MwaMhqz36G_!!6000000001499-73-tps-64-64.ico'
+                    icon: 'https://img.alicdn.com/imgextra/i2/O1CN01ZyAlrn1MwaMhqz36G_!!6000000001499-73-tps-64-64.ico',
                 },
                 {
                     title: '图标库iconPark',
                     url: 'https://iconpark.oceanengine.com/',
-                    icon:'https://lf1-cdn2-tos.bytegoofy.com/bydesign/iconparksite/logo.svg'
+                    icon: 'https://lf1-cdn2-tos.bytegoofy.com/bydesign/iconparksite/logo.svg',
                 },
                 {
                     title: '千库网',
@@ -53,15 +53,15 @@ const config = {
                     title: '高清图片pixabay',
                     url: 'https://pixabay.com/',
                 },
-            ]
+            ],
         },
         {
-            type:"资源查询",
+            type: '资源查询',
             links: [
                 {
                     title: 'Maven仓库',
                     url: 'https://mvnrepository.com/',
-                    icon:'https://mvnrepository.com/assets/images/7080b8b0f6f48e6fbaffd5f9d85fcc7f-favicon.ico'
+                    icon: 'https://mvnrepository.com/assets/images/7080b8b0f6f48e6fbaffd5f9d85fcc7f-favicon.ico',
                 },
                 {
                     title: '阿里镜像',
@@ -73,25 +73,25 @@ const config = {
                 },
                 {
                     title: 'chrome插件下载',
-                    url: 'http://cd.cduukj.com/'
+                    url: 'http://cd.cduukj.com/',
                 },
                 {
                     title: '哔哩哔哩视频下载',
-                    url: 'https://xbeibeix.com/api/bilibili/'
+                    url: 'https://xbeibeix.com/api/bilibili/',
                 },
-            ]
+            ],
         },
         {
-            type:"实用工具",
+            type: '实用工具',
             links: [
                 {
                     title: '程序员工具',
                     url: 'https://tool.p2hp.com/',
-                    icon: 'https://tool.p2hp.com/tools/images/icon.png'
+                    icon: 'https://tool.p2hp.com/tools/images/icon.png',
                 },
                 {
                     title: '在线PS',
-                    url: 'https://www.uupoop.com/'
+                    url: 'https://www.uupoop.com/',
                 },
                 {
                     title: '图片格式转换',
@@ -99,53 +99,57 @@ const config = {
                 },
                 {
                     title: 'cron表达式生成器',
-                    url: 'https://cron.qqe2.com/'
+                    url: 'https://cron.qqe2.com/',
                 },
-            ]
+            ],
         },
         {
-            type:"开发社区",
+            type: '开发社区',
             links: [
                 {
                     title: '码云Gitee',
-                    url: 'https://gitee.com/'
+                    url: 'https://gitee.com/',
                 },
                 {
                     title: 'Github',
-                    url: 'https://github.com/'
+                    url: 'https://github.com/',
                 },
                 {
                     title: 'CSDN',
-                    url: 'https://blog.csdn.net/'
+                    url: 'https://blog.csdn.net/',
                 },
                 {
                     title: 'Vue',
                     url: 'https://cn.vuejs.org/v2/guide/',
-                    icon: 'https://cn.vuejs.org/images/logo.svg'
+                    icon: 'https://cn.vuejs.org/images/logo.svg',
                 },
                 {
                     title: 'ShardingJDBC',
                     url: 'https://shardingsphere.apache.org/document/current/cn/overview/',
-                    icon: 'https://shardingsphere.apache.org/document/current/img/favicon.png'
+                    icon: 'https://shardingsphere.apache.org/document/current/img/favicon.png',
                 },
                 {
                     title: '若依官网',
                     url: 'http://www.ruoyi.vip/',
-                    icon: 'http://www.ruoyi.vip/images/favicon.ico'
+                    icon: 'http://www.ruoyi.vip/images/favicon.ico',
                 },
                 {
                     title: 'ElementUI',
                     url: 'https://element.eleme.cn/#/zh-CN/component/installation',
-                    icon: 'https://element.eleme.cn/favicon.ico'
+                    icon: 'https://element.eleme.cn/favicon.ico',
                 },
                 {
                     title: 'vxe-table',
                     url: 'https://vxetable.cn/v3/#/table/start/install',
                 },
-            ]
+                {
+                    title: 'ElasticSearch',
+                    url: 'https://www.elastic.co/cn/',
+                },
+            ],
         },
         {
-            type:"控制面板",
+            type: '控制面板',
             links: [
                 {
                     title: '宝塔Linux（106.53.125.228）',
@@ -158,9 +162,9 @@ const config = {
                 {
                     title: '宝塔Windows（梅老板）',
                     url: 'http://meiko.tzy.cool/:8888/aI3avzfc/',
-                }
-            ]
-        }
+                },
+            ],
+        },
     ],
-    websocketUrl: 'wss://110.40.159.23:8080/websocket'
-}
+    websocketUrl: 'wss://110.40.159.23:8080/websocket',
+};
