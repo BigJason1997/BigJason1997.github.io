194c2247487a7416343a6bfe71f75b386350d2a1
Jason1997 <921509429@qq.com>
2022-06-08 22:02:13 +0800

    修改聊天室修复工具地址

--- js/components/tiny-chat.js
+++ js/components/tiny-chat.js
@@ -1,6 +1,6 @@
-Vue.component('tinyChat',{
+Vue.component('tinyChat', {
     data() {
-        return{
+        return {
             // websocket对象
             websocket: null,
             // 输入的消息内容
@@ -10,85 +10,88 @@ Vue.component('tinyChat',{
             // 昵称
             nickname: '',
             // 消息列表
-            messageList: []
-        }
+            messageList: [],
+        };
     },
     created() {
-        this.setUserId()
-        this.initWebsocket()
+        this.setUserId();
+        this.initWebsocket();
     },
     methods: {
         // 设置userId
         setUserId() {
-            let bigJasonStorage = localStorage.getItem("bigJasonStorage")
-            try{
-                bigJasonStorage = JSON.parse(bigJasonStorage)
-                let userId = bigJasonStorage.userId
-                if(!userId) {
-                    userId = uuid()
+            let bigJasonStorage = localStorage.getItem('bigJasonStorage');
+            try {
+                bigJasonStorage = JSON.parse(bigJasonStorage);
+                let userId = bigJasonStorage.userId;
+                if (!userId) {
+                    userId = uuid();
                 }
-                let nickname = bigJasonStorage.nickname
-                if(!nickname) {
-                    nickname = userId
+                let nickname = bigJasonStorage.nickname;
+                if (!nickname) {
+                    nickname = userId;
                 }
-                localStorage.setItem("bigJasonStorage",JSON.stringify(bigJasonStorage))
+                localStorage.setItem('bigJasonStorage', JSON.stringify(bigJasonStorage));
             } catch (ex) {
-                console.error(ex)
-                let userId = uuid()
+                console.error(ex);
+                let userId = uuid();
                 bigJasonStorage = {
                     userId: userId,
-                    nickname: userId
-                }
-                localStorage.setItem("bigJasonStorage",JSON.stringify(bigJasonStorage))
+                    nickname: userId,
+                };
+                localStorage.setItem('bigJasonStorage', JSON.stringify(bigJasonStorage));
             }
-            this.userId = bigJasonStorage.userId
-            this.nickname = bigJasonStorage.nickname
+            this.userId = bigJasonStorage.userId;
+            this.nickname = bigJasonStorage.nickname;
         },
         // 初始化websocket
         initWebsocket() {
-            this.websocket = new WebSocket(config.websocketUrl + '?userId=' + this.userId)
-            this.websocket.onopen = this.handleOpenWebsocket
-            this.websocket.onclose = this.handleCloseWebsocket
-            this.websocket.onmessage = this.handleReceiveMessage
+            this.websocket = new WebSocket(config.websocketUrl + '?userId=' + this.userId);
+            this.websocket.onopen = this.handleOpenWebsocket;
+            this.websocket.onclose = this.handleCloseWebsocket;
+            this.websocket.onmessage = this.handleReceiveMessage;
         },
         // websocket连接成功钩子
         handleOpenWebsocket() {
-            console.log('聊天室连接成功')
+            console.log('聊天室连接成功');
         },
         // websocket断开钩子
         handleCloseWebsocket() {
-            console.log('聊天室已断开，正在重连')
-            setTimeout(this.initWebsocket,3000)
+            console.log('聊天室已断开，正在重连');
+            setTimeout(this.initWebsocket, 3000);
         },
         // websocket收到消息
         handleReceiveMessage(e) {
-            const message = JSON.parse(e.data.split(` [<a href='http://coolaf.com/tool/chattest'>http://coolaf.com</a>]`)[0])
-            message.time = new Date()
-            this.messageList.push(message)
+            const message = JSON.parse(
+                e.data.split(
+                    ` [<a href='http://coolaf.com/tool/chattest'>http://coolaf.com</a>]`
+                )[0]
+            );
+            message.time = new Date();
+            this.messageList.push(message);
             this.$nextTick(() => {
-                this.$refs['message-list'].scrollTop = this.$refs['message-list'].scrollHeight
-            })
+                this.$refs['message-list'].scrollTop = this.$refs['message-list'].scrollHeight;
+            });
         },
         packageMessage() {
-          return JSON.stringify({
-              userId: this.userId,
-              nickname: this.nickname,
-              message: this.messageInput
-          })
+            return JSON.stringify({
+                userId: this.userId,
+                nickname: this.nickname,
+                message: this.messageInput,
+            });
         },
         // 发送消息
         sendMessage() {
             try {
-                this.websocket.send(this.packageMessage())
-                this.messageInput = ''
+                this.websocket.send(this.packageMessage());
+                this.messageInput = '';
             } catch (ex) {
-                console.error(ex)
+                console.error(ex);
                 this.$message.error('消息发送失败');
             }
-        }
+        },
     },
-    template:
-        `
+    template: `
 <div class="home">
     <el-card class="box-card">
     <div class="message-list" ref="message-list">
@@ -112,7 +115,7 @@ Vue.component('tinyChat',{
                 <span style="padding: 10px">昵称</span>
             </template>
         </el-input>
-        <el-link type="primary" href="https://110.40.159.23:8080/" target="_blank">如无法连接聊天室，点击此处并继续访问后回到此页面</el-link>
+        <el-link type="primary" href="https://meiko.tzy.cool:8080/" target="_blank">如无法连接聊天室，点击此处并继续访问后回到此页面</el-link>
     </div>
     <el-input
       resize="none"
@@ -128,4 +131,4 @@ Vue.component('tinyChat',{
     </el-card>
 </div>
 `,
-})
+});
